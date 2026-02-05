0375bb20b4ad639b95a5d1dcaa90f6f6173b10b6
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-02-05 11:00:04 +0530

    Designing of Home page component has been done

--- LinkedInFrontend/src/pages/home/profileCard.tsx
+++ LinkedInFrontend/src/pages/home/profileCard.tsx
@@ -10,33 +10,30 @@ function ProfileCard (){
 
      <div className="profile-card">
 
-
-
             <Link className='links' to ="/profile">
 
-              <div className="banner-img">
-               Banner <img src="banner" alt="" />
-            </div>
+               <div className="profile-wrapper">
 
-            <div className="profile-img">
-             Profile Image  <img src="profile" alt="" />
-            </div>
+               <img className='banner-img' src="/src/assets/spacex.png" alt="" />
+
+             <img className='profile-img' src="/src/assets/Yashodeep.JPG" alt="" />
+            
+               </div>
+              
 
             <div className="profile-info">
                 <h3>Yashodeep Deshpande</h3>
                 <p>Aspiring Software Engineer | React • JavaScript | Turning Ideas into</p>
                 <address>Chhatrapati Sambhajinagar, Maharashtra</address>
             </div>
+
+
             </Link>
 
              <button className='btn'>+ Experince</button>
 
 
         </div>
-        
-
-
-
 </div>
        
         </>
