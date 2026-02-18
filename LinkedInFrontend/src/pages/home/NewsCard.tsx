02551f90be7001ba18e54f1925899e2fdc4a7f37
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-02-18 08:39:59 +0530

    Started working on News card component styling

--- LinkedInFrontend/src/pages/home/NewsCard.tsx
+++ LinkedInFrontend/src/pages/home/NewsCard.tsx
@@ -1,8 +1,33 @@
+import './NewsCard.css'
+import { Link } from 'react-router-dom'
 function NewsCard (){
     return (
         <>
 
-        <h4>This is a News Card Component</h4>
+        <div className="news-card-container">
+
+            <div className="news-card-header">
+                <h3>LinkedIn News</h3>
+                <button>i</button>
+            </div>
+
+            <div className="news-card-topstories">
+                <span>Top stories</span>
+                <Link to="/adani" className='stories-links'>  
+                <span>Adani Group bets $100B on AI</span>
+                </Link>
+                <Link to="/adani"className='stories-links'>  
+                <span>Adani Group bets $100B on AI</span>
+                </Link>
+                <Link to="/adani"className='stories-links'>  
+                <span>Adani Group bets $100B on AI</span>
+                </Link>
+                
+            <button className='news-card-showmore' >Show more</button>
+            </div>
+
+        
+        </div>
         
         </>
     )
