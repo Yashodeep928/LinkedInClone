c5f827e3b9b499e62a833f33d25152751ec0e9c1
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-02-20 16:07:30 +0530

    News card component is being successfully made

--- LinkedInFrontend/src/pages/home/NewsCard.tsx
+++ LinkedInFrontend/src/pages/home/NewsCard.tsx
@@ -1,13 +1,18 @@
 import './NewsCard.css'
 import { useState } from 'react';
-import { FaCaretDown, FaCaretUp} from 'react-icons/fa';
-import { Link } from 'react-router-dom'
+import { FaCaretDown, FaCaretUp, FaChevronRight} from 'react-icons/fa';
+import { Link } from "react-router-dom";
 function NewsCard (){
      
     const [show,setShow] = useState(false)
+    const[showpuzzle,setShowpuzzle] = useState(false)
 
     const openbox = ()=>{
         setShow(prev => !prev)
+    }
+
+    const openpuzzle = ()=>{
+      setShowpuzzle(prev => !prev)
     }
 
     return (
@@ -56,10 +61,37 @@ function NewsCard (){
     </>
   )}
 </button>
-             
+</div>
+
+         <div className="puzzlesection">
+          <span>Today's puzzle game</span>
+
+      <Link to='#' className='puzzleLinks'><span><img src="" alt="" /> Zip#340</span><FaChevronRight className='arrowicon'/></Link>
+      <Link to='#' className='puzzleLinks'><span><img src="" alt="" /> Zip#340</span><FaChevronRight className='arrowicon'/></Link>
+      <Link to='#' className='puzzleLinks'><span><img src="" alt="" /> Zip#340</span><FaChevronRight className='arrowicon'/></Link>
+
+      <div className={`puzzlelinkssection ${showpuzzle ?"active" : "" } `}>
+        <Link to='#' className='puzzleLinks'><span><img src="" alt="" /> Zip#340</span><FaChevronRight className='arrowicon'/></Link>
+      <Link to='#' className='puzzleLinks'><span><img src="" alt="" /> Zip#340</span><FaChevronRight className='arrowicon'/></Link>
+      <Link to='#' className='puzzleLinks'><span><img src="" alt="" /> Zip#340</span><FaChevronRight className='arrowicon'/></Link>
+
+      </div>
 
 
-            </div>
+
+      <button className='news-card-showpuzzle' onClick={openpuzzle}>{showpuzzle ? (
+    <>
+      Show Less <FaCaretUp />
+    </>
+  ) : (
+    <>
+      Show More <FaCaretDown />
+    </>
+  )}</button>
+    
+         
+
+         </div>
 
         
         </div>
