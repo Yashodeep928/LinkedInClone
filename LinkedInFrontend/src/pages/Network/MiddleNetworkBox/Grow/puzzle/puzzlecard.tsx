cde223c1ce98756e02b71a5314606a0617b1ff80
Yashodeep928 <yashodeepdeshpande84639@gmail.com>
2026-05-03 11:50:37 +0530

    Making Puzzle section

---
+++ LinkedInFrontend/src/pages/Network/MiddleNetworkBox/Grow/puzzle/puzzlecard.tsx
@@ -0,0 +1,34 @@
+import "./puzzlecard.css"
+function PuzzleCard() {
+  return (
+    <>
+   
+   <div className="puzzle-card">
+   
+     <div className="upper-row">
+     <p>Your morning puzzle is ready 🌄</p>
+
+     </div>
+
+
+        <div className="down-row">
+
+            <img src="" alt="" />
+             <div className="puzzle-info">
+               <p>Puzzle Info</p>
+             </div>
+
+             <button className="btn-1">Solve</button>
+
+
+        </div>
+
+   </div>
+
+    
+    
+    </>
+  )
+}
+
+export default PuzzleCard
--- LinkedInFrontend/src/pages/Network/MyNetwork.tsx
+++ LinkedInFrontend/src/pages/Network/MyNetwork.tsx
@@ -2,6 +2,7 @@ import Footer2 from '../../components/Footer/Footer2'
 import ManageNetwork from './ManageNetwork/ManageNetwork'
 import InviteCard from './MiddleNetworkBox/Grow/invitations/InviteCard'
 import NetworkBox from './MiddleNetworkBox/NetworkBox'
+import PuzzleCard from './MiddleNetworkBox/Grow/puzzle/puzzlecard'
 // import NetworkCard from './NetworkCard/NetworkCard'
 
 function MyNetwork() {
@@ -17,6 +18,7 @@ function MyNetwork() {
     <div className="middleSection">
       <NetworkBox/>
       <InviteCard/>
+      <PuzzleCard/>
     </div>    
     </>
   )
