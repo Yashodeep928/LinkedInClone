d44c615b60eb4def63459babb4b12330b2e05b0c
Yashodeep928 <yashodeepdeshpande84639@gmail.com>
2026-05-04 21:52:40 +0530

    Implementing caraousel effect

--- LinkedInFrontend/src/pages/Network/MiddleNetworkBox/Grow/puzzle/puzzlecard.tsx
+++ LinkedInFrontend/src/pages/Network/MiddleNetworkBox/Grow/puzzle/puzzlecard.tsx
@@ -10,18 +10,85 @@ function PuzzleCard() {
 
      </div>
 
+     <div className="parent-down-row">
 
-        <div className="down-row">
+        <div className="down-row2">
 
-            <img src="" alt="" />
+            <img src="" alt="image" />
+
              <div className="puzzle-info">
                <p>Puzzle Info</p>
+               <p>Patches</p>
+             </div>
+
+             <button className="btn-1">Solve</button>
+        </div>
+
+
+
+
+
+         <div className="down-row2">
+
+            <img src="" alt="image" />
+
+             <div className="puzzle-info">
+               <p>Puzzle Info</p>
+               <p>Patches</p>
+             </div>
+
+             <button className="btn-1">Solve</button>
+        </div>
+
+
+
+         <div className="down-row2">
+
+            <img src="" alt="image" />
+
+             <div className="puzzle-info">
+               <p>Puzzle Info</p>
+               <p>Patches</p>
              </div>
 
              <button className="btn-1">Solve</button>
 
+        </div>
+
+
+        <div className="down-row2">
+
+            <img src="" alt="image" />
+
+             <div className="puzzle-info">
+               <p>Puzzle Info</p>
+               <p>Patches</p>
+             </div>
+
+             <button className="btn-1">Solve</button>
 
         </div>
+
+
+
+        <div className="down-row2">
+
+            <img src="" alt="image" />
+
+             <div className="puzzle-info">
+               <p>Puzzle Info</p>
+               <p>Patches</p>
+             </div>
+
+             <button className="btn-1">Solve</button>
+
+        </div>
+
+
+     </div>
+
+
+        
 
    </div>
 
