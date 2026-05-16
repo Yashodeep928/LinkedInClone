95b015f1b819340add783afa2e5f29fff64529aa
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-05-16 17:55:35 +0530

    Started Working on the Jobs Section

--- LinkedInFrontend/src/App.tsx
+++ LinkedInFrontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import Home from "./pages/home/Home";
 import MyNetwork from "./pages/Network/MyNetwork";
+import Jobs from "./pages/Jobs/jobs";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Route path="/" element={<Layout />}>
     <Route index element={<Home />} />
     <Route path="/network" element={<MyNetwork />} />
+    <Route path="/jobs" element={<Jobs/>} />
     </Route>
     </Routes>
   );
---
+++ LinkedInFrontend/src/pages/Jobs/jobs.tsx
@@ -0,0 +1,21 @@
+import Footer2 from '../../components/Footer/Footer2'
+import ProfileCard from '../home/profileCard/profileCard'
+import './jobs.css'
+function Jobs() {
+  return (
+    <>
+      <div className="jobs-sidebar">
+       <ProfileCard/>
+        <Footer2/>
+
+    </div>
+
+    <div className="jobs-main">
+  
+    </div>    
+    
+    </>
+  )
+}
+
+export default Jobs
