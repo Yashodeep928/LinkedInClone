f89188d3f36b94cc9811fe628820955b29a9db71
Yashodeep928 <yashodeepdeshpande84639@gmail.com>
2026-05-18 22:49:12 +0530

    Worked on enhancements of JobsPicks page

--- LinkedInFrontend/src/pages/Jobs/Jobpicks/JobPicks.tsx
+++ LinkedInFrontend/src/pages/Jobs/Jobpicks/JobPicks.tsx
@@ -71,6 +71,38 @@ function JobPicks() {
                 
             </div>
 
+
+            <div className="jobs-content">
+
+
+            <div className="jobs-card">
+
+                <div className="left">
+
+                    <img src="image" alt="image" />
+
+                    <div className="job-content">
+                       <h3>React Developer</h3>
+                        <p>Reacr Developer</p>
+                        <p>Reacr Developer</p>
+                    </div>
+
+                </div>
+                
+
+
+                  <div className="right">
+
+                       <button className="cross-btn"><X color="black" size={22} /></button>
+
+                  </div>
+                
+            </div>
+
+           
+
+        </div>
+
            
 
         </div>
