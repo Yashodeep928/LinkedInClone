b309ae9a04d7fa56717f62c11c875946537a20ac
Yashodeep928 <yashodeepdeshpande84639@gmail.com>
2026-05-19 23:12:50 +0530

    Made More Jobs Section

--- LinkedInFrontend/src/pages/Jobs/Jobpicks/JobPicks.tsx
+++ LinkedInFrontend/src/pages/Jobs/Jobpicks/JobPicks.tsx
@@ -6,8 +6,8 @@ function JobPicks() {
     <div className="jobs-section">
 
         <div className="jobs-header">
-             <p>Top job picks for you</p>
-             <p>Based on your profile, preferences, and activity like applies, searches, and saves</p>
+             <p className="para">Top job picks for you</p>
+             <p className="small-para">Based on your profile, preferences, and activity like applies, searches, and saves</p>
         </div>
 
 
@@ -116,3 +116,124 @@ function JobPicks() {
 }
 
 export default JobPicks
+
+
+export function MoreJobs (){
+
+    return(
+        <>
+
+        <div className="jobs-section">
+
+        <div className="jobs-header">
+             <p className="para">More jobs for you</p>
+             <p className="small-para">Based on your profile, preferences, and activity like applies, searches, and saves</p>
+        </div>
+
+
+
+        <div className="jobs-content">
+
+
+            <div className="jobs-card">
+
+                <div className="left">
+
+                    <img src="image" alt="image" />
+
+                    <div className="job-content">
+                       <h3>React Developer</h3>
+                        <p>Reacr Developer</p>
+                        <p>Reacr Developer</p>
+                    </div>
+
+                </div>
+                
+
+
+                  <div className="right">
+
+                       <button className="cross-btn"><X color="black" size={22} /></button>
+
+                  </div>
+                
+            </div>
+
+           
+
+        </div>
+
+
+         <div className="jobs-content">
+
+
+            <div className="jobs-card">
+
+                <div className="left">
+
+                    <img src="image" alt="image" />
+                    
+                    <div className="job-content">
+                       <h3>React Developer</h3>
+                        <p>Reacr Developer</p>
+                        <p>Reacr Developer</p>
+                    </div>
+
+                </div>
+                
+
+
+                  <div className="right">
+
+                       <button className="cross-btn"><X color="black" size={22} /></button>
+
+                  </div>
+                
+            </div>
+
+
+            <div className="jobs-content">
+
+
+            <div className="jobs-card">
+
+                <div className="left">
+
+                    <img src="image" alt="image" />
+
+                    <div className="job-content">
+                       <h3>React Developer</h3>
+                        <p>Reacr Developer</p>
+                        <p>Reacr Developer</p>
+                    </div>
+
+                </div>
+                
+
+
+                  <div className="right">
+
+                       <button className="cross-btn"><X color="black" size={22} /></button>
+
+                  </div>
+                
+            </div>
+
+           
+
+        </div>
+
+           
+
+        </div>
+
+
+    </div>
+        
+        </>
+    )
+
+    
+
+}
+
