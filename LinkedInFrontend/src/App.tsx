97da1262c4f51204252331393f095bffc37005d7
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-05-21 10:39:32 +0530

    Started Working on Notifications

--- LinkedInFrontend/src/App.tsx
+++ LinkedInFrontend/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout/layout";
 import Home from "./pages/home/Home";
 import MyNetwork from "./pages/Network/MyNetwork";
 import Jobs from "./pages/Jobs/jobs";
+import Notification from "./pages/Notification/Notification";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <Route index element={<Home />} />
     <Route path="/network" element={<MyNetwork />} />
     <Route path="/jobs" element={<Jobs/>} />
+    <Route path="/notifications" element={<Notification/>}/>
     </Route>
     </Routes>
   );
---
+++ LinkedInFrontend/src/pages/Notification/Notification.tsx
@@ -0,0 +1,35 @@
+import Footer from "../../components/Footer/Footer"
+import ProfileCard from "../home/profileCard/profileCard"
+
+function Notification() {
+
+  return (
+
+    <>
+
+<div className="profileSection">
+    <ProfileCard/>
+</div>
+
+
+<div className="middleSection">
+
+</div>
+
+<div className="rightSection">
+    <Footer/>
+</div>
+
+
+
+ 
+
+    
+    
+    </>
+
+
+  )
+}
+
+export default Notification
