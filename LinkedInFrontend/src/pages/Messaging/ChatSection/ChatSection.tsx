bbe7c450160bb4cb06b64d54cda07539d26f2afb
Yashodeep Deshpande <yashodeepdeshpande84639@gmail.com>
2026-06-02 14:46:26 +0530

    Working on the Attachment toolbar

--- LinkedInFrontend/src/pages/Messaging/ChatSection/ChatSection.tsx
+++ LinkedInFrontend/src/pages/Messaging/ChatSection/ChatSection.tsx
@@ -2,10 +2,20 @@ import "./ChatSection.css"
 import { Ellipsis,Edit2, Star,  Paperclip,
   Image,
   Smile,
-  Send,
-  FileText } from "lucide-react"
+  Film} from "lucide-react"
+
+  import {useState} from "react"
 
 function ChatSection() {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>("")
+  
+
+  const openModal = () =>{
+
+    setIsOpen((prev) => !prev)
+
+  }
   return (
     <>
     <div className="messaging-chat-section">
@@ -93,10 +103,50 @@ function ChatSection() {
                   </div>
 
                   <div className="Attachment-Toolbar">
-                   <button>  <Paperclip /> </button>
-                   <button>  <Image /> </button>
-                   <button>  <Smile /> </button>
-                   <button>  <FileText /> </button>
+
+                   <div className="left-Attachment-actions">
+
+                      <button>  <Paperclip/> </button>
+                      <button>  <Image/>     </button>
+                      <button>  <Smile/>     </button>
+                      <button>  <Film/>      </button>
+
+                   </div>
+
+                   <div className="right-Attachment-actions">
+                        <span>{message}</span>
+                       <button onClick={openModal}><Ellipsis/></button>
+
+                       {
+                        isOpen && (
+                          <div className ="attachment-dropdown">
+
+
+                          <div className="press-enter">
+
+                              <input type="checkbox" id="press-enter" value="Press Enter to Send"  onChange={(e) =>{ if(e.target.checked){
+                                     setMessage(e.target.value)
+                              }}}  />
+                              <label htmlFor="press-enter">Press Enter to Send</label>
+
+                          </div>
+
+                           <div className="click-to-send">
+
+                              <input type="checkbox" id="click-to-send" value="Click to send" onChange={(e) =>{ if(e.target.checked){
+                                     setMessage(e.target.value)
+                              }}}  />
+                              <label htmlFor="click-to-send">Click to send</label>
+                            
+                           </div>
+                           
+
+                            <button onClick={()=>setIsOpen(!isOpen)} type="button">CUT</button>
+                          </div>
+                        )
+                       }
+
+                   </div>
                    
                                    
                   </div>
