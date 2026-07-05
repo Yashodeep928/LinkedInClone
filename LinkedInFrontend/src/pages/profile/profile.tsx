278b075aa00096a6bbf8957f6ba895095e097169
Yashodeep928 <yashodeepdeshpande84639@gmail.com>
2026-07-05 18:38:19 +0530

    Started working on the profile section

--- LinkedInFrontend/src/pages/profile/profile.tsx
+++ LinkedInFrontend/src/pages/profile/profile.tsx
@@ -2,6 +2,29 @@
 function profile() {
   return (
     <>
+    <div className="profile-section">
+
+      <div className="profile-left">
+
+        <div className="profile-summary">
+
+        </div>
+
+        <div className="profile-about">
+
+        </div>
+
+        <div className="profile-activity">
+          
+        </div>
+
+      </div>
+
+      <div className="profile-right">
+
+      </div>
+
+    </div>
     
 
     
